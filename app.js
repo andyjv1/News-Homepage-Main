444d0a89845a92da71bb134d6cd9e5c2a88f0711
andyjv1 <111096875+andyjv1@users.noreply.github.com>
2023-05-30 20:50:46 -0400

    update HTML tags and css convention

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 
-
-document.getElementById("icon-menu").addEventListener('click', mobileMenu2)
-
 document.getElementById("icon-menu-close").addEventListener('click', mobileMenu1)
+document.getElementById("icon-menu").addEventListener('click', mobileMenu2)
+document.getElementById("main-overlay").addEventListener('click', mobileMenu3)
 
 function mobileMenu1() {
-        document.getElementById("myLinks").style.right = '-16rem'
-        document.getElementById("overlay").style.display = 'none'
+        document.getElementById("main-links").style.right = '-16rem'
+        document.getElementById("main-overlay").style.display = 'none'
 }
 
 function mobileMenu2() {
-        document.getElementById("myLinks").style.right = '0'
-        document.getElementById("overlay").style.display = 'block'
+        document.getElementById("main-links").style.right = '0'
+        document.getElementById("main-overlay").style.display = 'block'
+}
 
+function mobileMenu3() {
+        document.getElementById("main-links").style.right = '-16rem'
+        document.getElementById("main-overlay").style.display = 'none'
 }
